f660fd84ef4e997517ca97c0254f436db6b39e0f
Hotosho <nicksecond1995@gmail.com>
2019-12-10 01:31:33 +0100

    #

--- src/lesson32/Homework/Solution.java
+++ src/lesson32/Homework/Solution.java
@@ -16,26 +16,38 @@ public class Solution {
 
         int countErrors = 3;
         int sum = 0;
+        int tryEnter = 10;
 
         while (countErrors > 0) {
+            while (tryEnter > 0) {
 
-            String numbers = bf.readLine();
+                String numbers = bf.readLine();
 
-            if (checkNumbers(numbers)) {
-                String[] arrayNumbers = numbers.split(" ");
-                for (String str : arrayNumbers) {
-                    int x = Integer.parseInt(str);
-                    sum += x;
+                if (checkNumbers(numbers)) {
+                    String[] arrayNumbers = numbers.split(" ");
+
+                    if (arrayNumbers.length > 10)
+                        break;
+
+                    for (String str : arrayNumbers) {
+                        int x = Integer.parseInt(str);
+                        sum += x;
+                    }
+                    System.out.println("Sum your numbers +" + sum);
                 }
-                System.out.println("Sum your numbers +" + sum);
-            }
 
-            if (!checkNumbers(numbers)) {
-                countErrors--;
-                if (countErrors == 0) {
-                    System.out.println("Your numbers are wrong. You have" + countErrors + "attempts to try again");
-                } else {
-                    System.out.println("Your numbers are wrong. Number of attempts exceeded");
+                if (!checkNumbers(numbers)) {
+                    countErrors--;
+                    if (countErrors == 0) {
+                        System.out.println("Your numbers are wrong. You have" + countErrors + "attempts to try again");
+                    } else {
+                        System.out.println("Your numbers are wrong. Number of attempts exceeded");
+                    }
+                }
+                tryEnter--;
+                if (tryEnter == 0){
+                    System.out.println("maximum number of digits entered");
+                    break;
                 }
             }
         }
